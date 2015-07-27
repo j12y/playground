bd6472903c3787952b00c13be2bfe81d9dc4240a
j12y <jayson.delancey@gmail.com>
2015-07-26 21:21:54 -0700

    app through ch 6

--- mean/app/gulpfile.js
+++ mean/app/gulpfile.js
@@ -1,16 +1,16 @@
 var gulp = require('gulp');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var ngAnnotate = require('gulp-ng-annotate');
+var fs = require('fs');
 
-gulp.task('js', function() {
-    gulp.src(['ng/module.js', 'ng/**/*.js'])
-        .pipe(concat('app.js'))
-        .pipe(ngAnnotate())
-        .pipe(uglify())
-        .pipe(gulp.dest('assets'))
+fs.readdirSync(__dirname + '/gulp').forEach(function(task) {
+    require('./gulp/' + task);
 });
 
-gulp.task('watch:js', ['js'], function() {
+gulp.task('watch:js', function() {
     gulp.watch('ng/**/*.js', ['js']);
 });
+
+gulp.task('watch:css', function() {
+    gulp.watch('css/**/*.styl', ['css']);
+});
+
+gulp.task('dev', ['watch:css', 'watch:js', 'dev:server']);
