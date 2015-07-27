413f5b7d428ebd3ee89735eaeb5fcc5023b5a97d
j12y <jayson.delancey@gmail.com>
2015-07-26 20:08:01 -0700

    next iteration of chapter changes

--- mean/app/gulpfile.js
+++ mean/app/gulpfile.js
@@ -1,9 +1,16 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var ngAnnotate = require('gulp-ng-annotate');
 
-gulp.task('welcome', function() {
-    console.log('welcome to gulp!');
+gulp.task('js', function() {
+    gulp.src(['ng/module.js', 'ng/**/*.js'])
+        .pipe(concat('app.js'))
+        .pipe(ngAnnotate())
+        .pipe(uglify())
+        .pipe(gulp.dest('assets'))
 });
 
-gulp.task('hello', ['welcome'], function() {
-    console.log('hello world');
+gulp.task('watch:js', ['js'], function() {
+    gulp.watch('ng/**/*.js', ['js']);
 });
